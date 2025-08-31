e41b4152459cad71e46710f538f5bb292204b923
Jacob Canedy <89418437+JacobACan@users.noreply.github.com>
2025-08-31 15:22:13 -0400

    updated notes and cadences

---
+++ app/app/constants/cadences.tsx
@@ -0,0 +1,14 @@
+export enum cadenceFiles {
+  A4 = "A4.ogg",
+  Ab4 = "Ab4.ogg",
+  B4 = "B4.ogg",
+  Bb4 = "Bb4.ogg",
+  C4 = "C4.ogg",
+  D4 = "D4.ogg",
+  Db4 = "Db4.ogg",
+  E4 = "E4.ogg",
+  Eb4 = "Eb4.ogg",
+  F4 = "F4.ogg",
+  G4 = "G4.ogg",
+  Gb4 = "Gb4.ogg",
+}
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,17 +3,27 @@
 import { MIDIProvider } from "@react-midi/hooks"
 import { MidiSelector } from "./components/midi"
 import Piano from "./components/piano"
-import { midiToNote, Note, noteFilesArray, noteFilesToNotes } from "./constants/notes"
+import {
+  midiToNote,
+  Note,
+  noteFiles,
+  noteFilesArray,
+  noteFilesToNotes,
+} from "./constants/notes"
 import { useState } from "react"
+import { cadenceFiles } from "./constants/cadences"
 
 export default function Home() {
-
-
   // Helper to play a note by filename
-  const playNote = (noteFile: string) => {
+  const playNote = (noteFile: noteFiles) => {
     const audio = new Audio(`/notes/${noteFile}`)
     audio.play()
-  } 
+  }
+  // Helper to play a note by filename
+  const playCadence = (cadenceFile: cadenceFiles) => {
+    const audio = new Audio(`/cadences/${cadenceFile}`)
+    audio.play()
+  }
 
   //currentNote usestate var
   const [currentNote, setCurrentNote] = useState<Note | null>(null)
@@ -21,40 +31,40 @@ export default function Home() {
 
   const getNextQuestionNote = () => {
     // play Random note from public/notes
-    
-    const random = noteFilesArray[Math.floor(Math.random() * noteFilesArray.length)]
-    playNote(random)
-    setQuestionNote(noteFilesToNotes[random]||null)
-  }
 
+    const random =
+      noteFilesArray[Math.floor(Math.random() * noteFilesArray.length)]
+    playNote(random)
+    setQuestionNote(noteFilesToNotes[random] || null)
+  }
 
   return (
     <MIDIProvider>
-
       <div className="flex flex-col items-center justify-center min-h-screen py-2 font-sans">
         <button
           className="mb-4"
           onClick={() => {
             // play C3 from public/notes
-            playNote("C3.ogg")
+            playCadence(cadenceFiles.C4)
           }}
         >
           Play Cadence
         </button>
         <button
-        className="mb-4"
+          className="mb-4"
           onClick={() => {
             // play Random note from public/notes
-            
-            const random = noteFilesArray[Math.floor(Math.random() * noteFilesArray.length)]
+
+            const random =
+              noteFilesArray[Math.floor(Math.random() * noteFilesArray.length)]
             playNote(random)
-            setQuestionNote(noteFilesToNotes[random]||null)
+            setQuestionNote(noteFilesToNotes[random] || null)
           }}
         >
           Play Random Note
         </button>
-        <Piano 
-          displayRange={[Note.C3, Note.C5]}
+        <Piano
+          displayRange={[Note.C4, Note.D5]}
           notesDown1={[...(currentNote ? [currentNote] : [])]}
           notesDown2={[...(questionNote ? [questionNote] : [])]}
           width={500}
@@ -72,10 +82,8 @@ export default function Home() {
                 console.log("Correct!")
                 getNextQuestionNote()
               }
-
             }
-          }
-          }
+          }}
         />
       </div>
     </MIDIProvider>
