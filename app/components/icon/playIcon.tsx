2f499151d5db7c3c0d906cfd94f35ffa297df926
Jacob Canedy <jcanedy19@gmail.com>
2025-11-06 17:04:21 -0500

    midi devices selector

--- app/components/icon/playIcon.tsx
+++ app/components/icon/playIcon.tsx
@@ -6,17 +6,14 @@ export default function PlayIcon({ width = 50, height = 50 }: IconProps) {
       width={width}
       height={width}
       viewBox="0 0 50 50"
-      fill="none"
-      stroke="var(--middleground1)"
       strokeWidth="2.5"
       strokeLinecap="round"
       strokeLinejoin="round"
     >
       <g transform="translate(25, 25) scale(2.3) translate(-25, -25)">
         <polygon
+          className="fill-accent stroke-accent"
           points="20,16 34,24 20,32"
-          fill="var(--middleground1)"
-          stroke="var(--middleground1)"
           strokeLinejoin="round"
         />
       </g>
--- app/components/learningApproachUI/learningApproach.tsx
+++ app/components/learningApproachUI/learningApproach.tsx
@@ -10,29 +10,79 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "../ui/carousel";
-import { LearningMode, LearningModeValues } from "@/constants/settings";
-import useEmblaCarousel from "embla-carousel-react";
+import { LearningModeValues } from "@/constants/settings";
+import { useMIDIInputs, useMIDINote } from "@react-midi/hooks";
+import { midiToNote, Note } from "@/constants/notes";
+import LearningUserEvent from "../learn/learningUserEvent";
+import PlayIcon from "../icon/playIcon";
+import { MusicLearnerEvent } from "@/machines/musicLearningProcess";
 
 export default function LearningApproach() {
-  const { settings, updateSettings } = useContext(SettingsContext);
-  const [api, setApi] = useState<CarouselApi>();
+  const { updateSettings, settings } = useContext(SettingsContext);
+  const [learningModeCarouselApi, setLearningModeCarouselApi] =
+    useState<CarouselApi>();
+  const { inputs, selectInput, selectedInputId, input } = useMIDIInputs();
+  const [midiDeviceCarouselApi, setMidiDeviceCarouselApi] =
+    useState<CarouselApi>();
+  const note = useMIDINote();
+  const [midiDeviceVerified, setMidiDeviceVerified] = useState(false);
+  const [midiDeviceStartIndex, setMidiDeviceStartIndex] = useState(0);
+  const [learningModeStartIndex, setLearningModeStartIndex] = useState(0);
 
   useEffect(() => {
-    if (!api) return;
+    if (!learningModeCarouselApi) return;
 
-    api.on("select", (e) => {
+    learningModeCarouselApi.on("select", (e) => {
       updateSettings(
         "learningMode",
-        LearningModeValues[e.selectedScrollSnap()]
+        LearningModeValues[e.selectedScrollSnap()],
       );
     });
-  }, [api]);
+  }, [learningModeCarouselApi]);
+
+  useEffect(() => {
+    if (!midiDeviceCarouselApi) return;
+    if (!inputs || inputs.length == 0) return;
+    if (!setMidiDeviceVerified) return;
+    midiDeviceCarouselApi.on("select", (e) => {
+      if (e.selectedScrollSnap() > 0) {
+        selectInput(inputs[e.selectedScrollSnap() - 1].id);
+      }
+      setMidiDeviceVerified(false);
+    });
+  }, [midiDeviceCarouselApi, inputs, setMidiDeviceVerified]);
+
+  useEffect(() => {
+    if (!midiDeviceCarouselApi || !input) return;
+    const inputIndex = inputs.indexOf(input) + 1;
+    midiDeviceCarouselApi.scrollTo(inputIndex);
+  }, [inputs]);
+
+  useEffect(() => {
+    if (note && !midiDeviceVerified) setMidiDeviceVerified(true);
+  }, [note]);
+
+  useEffect(() => {
+    if (inputs.length == 0 || !input) return;
+
+    setMidiDeviceStartIndex(inputs.indexOf(input) + 1);
+    setMidiDeviceVerified(true);
+
+    if (!settings) return;
+    setLearningModeStartIndex(
+      LearningModeValues.indexOf(settings.learningMode),
+    );
+  }, []);
 
   const renderMainSettings = () => {
     return (
-      <div className="align-middle items-center text-center  h-[100vh] content-center place-items-center">
+      <div className="h-lvh content-center justify-items-center text-center">
         <h1 className="text-sm">Learning Mode</h1>
-        <Carousel setApi={setApi} opts={{ loop: true }} className=" w-[150px] ">
+        <Carousel
+          setApi={setLearningModeCarouselApi}
+          opts={{ loop: true, startIndex: learningModeStartIndex }}
+          className="w-[150px]"
+        >
           <CarouselContent>
             {LearningModeValues.map((m) => (
               <CarouselItem key={m}>{m}</CarouselItem>
@@ -41,20 +91,62 @@ export default function LearningApproach() {
           <CarouselPrevious variant={"justIntonnation"} />
           <CarouselNext variant={"justIntonnation"} />
         </Carousel>
-        <Piano></Piano>
+        <Carousel
+          setApi={setMidiDeviceCarouselApi}
+          opts={{
+            loop: true,
+            startIndex: midiDeviceStartIndex,
+          }}
+          className="w-[150px] text-sm"
+        >
+          <CarouselContent>
+            <CarouselItem className="content-center">
+              No Midi Device Selected
+            </CarouselItem>
+            {inputs.map((m) => (
+              <CarouselItem className="content-center" key={m.id}>
+                {m.name}
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious variant={"justIntonnation"} />
+          <CarouselNext variant={"justIntonnation"} />
+        </Carousel>
+        {!midiDeviceVerified ? (
+          <p className="text-destructive m-2 text-lg">
+            Test your midi device before starting!
+          </p>
+        ) : (
+          <p className="m-2 text-lg">All good to start learning!</p>
+        )}
+        <section className="absolute h-[350px] w-[500px] content-center drop-shadow-lg">
+          {midiDeviceVerified && (
+            <LearningUserEvent
+              className="m-auto"
+              eventType={MusicLearnerEvent.START}
+            >
+              <PlayIcon width={150} height={150} />
+            </LearningUserEvent>
+          )}
+        </section>
+        <Piano
+          notesDown1={note && note.on ? [midiToNote[note.note]] : []}
+          displayRange={[Note.C4, Note.C5]}
+          width={500}
+        ></Piano>
       </div>
     );
   };
 
   return (
-    <div className=" overflow-hidden">
-      <section className="w-[40%] h-[100vh] left-0 absolute">
-        <div className="absolute w-full h-full from-transparent via-background to-background bg-gradient-to-l "></div>
-        <div className="left-0 absolute p-4">General Settings</div>
+    <div className="overflow-hidden">
+      <section className="pointer-events-none absolute left-0 h-[100vh] w-[40%]">
+        <div className="via-background to-background pointer-events-none absolute h-full w-full bg-gradient-to-l from-transparent"></div>
+        <div className="absolute left-0 p-4">General Settings</div>
       </section>
-      <section className="w-[40%] h-[100vh] right-0 absolute">
-        <div className="absolute w-full h-full from-transparent via-background to-background bg-gradient-to-r "></div>
-        <div className="right-0 absolute p-4">Mode Settings</div>
+      <section className="pointer-events-none absolute right-0 h-[100vh] w-[40%]">
+        <div className="via-background to-background pointer-events-none absolute h-full w-full bg-gradient-to-r from-transparent"></div>
+        <div className="absolute right-0 p-4">Mode Settings</div>
       </section>
       {renderMainSettings()}
     </div>
